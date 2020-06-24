c92e938e9d26548eb950759953d6b49180919f78
patilmayank <67300824+patilmayank@users.noreply.github.com>
2020-06-23 18:46:45 -0700

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,11 @@
+sumofsquare=0
+for i in range(1,101):
+  sumofsquare+=i*i
+print (sumofsquare)
+total=0
+for i in range(1,101):
+  total+=i
+squareofsum=total*total
+print(total)
+print(squareofsum)
+print (squareofsum-sumofsquare)
